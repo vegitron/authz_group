1b0c3410f2f59e0768d19d8ed17c29d2993afa69
Patrick Michaud <pmichaud@uw.edu>
2014-01-16 14:24:15 -0800

    Making this uploadable to pypi as an egg

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='AuthZ-Group',
-      version='1.0',
-      description='Group implementations',
-      install_requires=['Django'],
-     )
+setup(
+    name='AuthZ-Group',
+    version='1.1',
+    description='Group interface and implementations',
+    packages = find_packages(),
+    install_requires=['Django'],
+    author = "Patrick Michaud",
+    author_email = "pmichaud@uw.edu",
+    license = "Apache 2.0",
+    keywords = "django groups authorization",
+    url = "https://github.com/vegitron/authz_group/"
+)
